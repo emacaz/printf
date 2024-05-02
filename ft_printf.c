d3d9ca5ce1746d81f716a8a1101ffce823832638
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-05-02 13:49:09 +0200

    Add libft-dir and start testing formats c, s

--- ft_printf.c
+++ ft_printf.c
@@ -6,25 +6,62 @@
 /*   By: emcastil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 11:34:29 by emcastil          #+#    #+#             */
-/*   Updated: 2024/05/02 12:28:40 by emcastil         ###   ########.fr       */
+/*   Updated: 2024/05/02 13:47:32 by emcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	ft_putchar(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr(char *s, int fd)
+{
+	size_t	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
 
 int	ft_printf(char const *format, ...)
 {
 	va_list	args;
 	int		count;
 
+	count = 0;
 	va_start(args, format);
-	count = vprintf(format, args);
+	while (*format)
+	{
+		if (*format == 'c')
+		{
+			ft_putchar(va_arg(args, int), 1);
+			count++;
+		}
+		if (*format == 's')
+		{
+			ft_putstr(va_arg(args, char *), 1);
+			count++;
+		}
+		format++;
+	}
 	va_end(args);
 	return (count);
 }
 
 int	main(void)
 {
-	ft_printf("Hola, $s!\n", "mundo");
+	char	letra = 'y';
+	char	*word = "hello";
+
+	ft_printf("%c", letra);
+	ft_printf("%s", word);
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,17 @@
 /*   By: emcastil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 12:14:53 by emcastil          #+#    #+#             */
-/*   Updated: 2024/05/02 12:16:21 by emcastil         ###   ########.fr       */
+/*   Updated: 2024/05/02 13:22:05 by emcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+#include "./libft/libft.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int	ft_printf(char const *, ...);
 
