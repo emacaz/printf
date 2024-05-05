d3b634e586476ef1a11088bcdb871e3cfc306ecf
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-05-05 22:15:32 +0200

    Warning functions

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -21,4 +21,10 @@ int		ft_printf(char const *format, ...);
 void	ft_putchar(char c, int fd);
 void	ft_putstr(char *s, int fd);
 
+/* WARNINGS */
+void	more_conv_th_d(int fd);
+void	data_not_used(int fd);
+void	inva_conv_spec(int fd);
+void	form_spec_type(int fd);
+
 #endif
